c9256a0ebc2d877f8b4cad82bd69f87f514c4db5
taji <taji@sonicgarden.jp>
2025-03-19 15:03:48 +0900

    nameバリデーションついか

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable
+
+  validates :name, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
 end
