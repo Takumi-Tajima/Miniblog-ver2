4195bb6316bbda7efbe6c30349b3673c165cf279
taji <taji@sonicgarden.jp>
2025-03-24 14:46:45 +0900

    user factoriesの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.first_name.downcase }
+    email { Faker::Internet.unique.email }
+    password { 'password' }
+    confirmed_at { Time.current }
+
+    trait :without_confirmation do
+      confirmed_at { nil }
+    end
   end
 end
