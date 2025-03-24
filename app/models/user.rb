6835d92ab474a708ac588c68f7e4d19f4a88ce37
taji <taji@sonicgarden.jp>
2025-03-24 15:03:37 +0900

    postsのリレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable, :confirmable
 
+  has_many :posts, dependent: :destroy
+
   validates :name, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
 end
