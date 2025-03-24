d57a115035df4f5e736a57855de2d668d46cc18c
taji <taji@sonicgarden.jp>
2025-03-24 15:02:02 +0900

    posts migration

--- db/migrate/20250324060000_create_posts.rb
+++ db/migrate/20250324060000_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :content
+      t.string :content, null: false, limit: 140
 
       t.timestamps
     end
