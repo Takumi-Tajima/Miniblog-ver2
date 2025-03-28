4d82e16388e949a2db5cc2ee574732661ff67206
taji <taji@sonicgarden.jp>
2025-03-28 13:27:09 +0900

    blog_urlのバリデーションを外し、profilrからbiographyに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :name, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
-  validates :profile, length: { maximum: 200 }
-  validates :blog_url, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/ }
+  validates :biography, length: { maximum: 200 }
 end
