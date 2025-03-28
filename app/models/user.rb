88f46e3a20ab74b054aa616e6239315861a60331
taji <taji@sonicgarden.jp>
2025-03-28 10:06:06 +0900

    profiletとblog_urlのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   validates :name, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
+  validates :profile, length: { maximum: 200 }
+  validates :blog_url, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/ }
 end
