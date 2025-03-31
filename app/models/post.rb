feddb3c72b19b781013a3b612990d68ab5a9e404
taji <taji@sonicgarden.jp>
2025-03-31 21:00:53 +0900

    liked_byメソッド追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
 
   scope :default_order, -> { order(:id) }
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
