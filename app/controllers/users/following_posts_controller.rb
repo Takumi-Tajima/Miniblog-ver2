85dda67e14a10faa756213532063fd9fdecfbef6
taji <taji@sonicgarden.jp>
2025-03-31 16:18:55 +0900

    タイポ修正

--- app/controllers/users/following_posts_controller.rb
+++ app/controllers/users/following_posts_controller.rb
@@ -1,7 +1,5 @@
 class Users::FollowingPostsController < Users::ApplicationController
   def show
-    @posts = Post.joins('INNER JOIN follows ON posts.user_id = follows.followed_id')
-                 .where(follows: { follower_id: current_user.id })
-                 .default_order
+    @posts = Post.where(user_id: current_user.following.pluck(:id))
   end
 end
