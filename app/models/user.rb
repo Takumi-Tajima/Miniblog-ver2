5653aa8076974391384287e9647af49553ac9bdd
taji <taji@sonicgarden.jp>
2025-03-31 11:13:26 +0900

    リレーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable, :confirmable
 
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
 
   validates :name, format: { with: /\A[a-zA-Z0-9]+\z/ }, length: { maximum: 20 }
   validates :biography, length: { maximum: 200 }
