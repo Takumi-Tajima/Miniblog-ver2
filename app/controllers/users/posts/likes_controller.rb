71f44392824c6d29dd2934bf5a57fcd557488956
taji <taji@sonicgarden.jp>
2025-03-31 23:27:39 +0900

    いいねしたユーザーの一覧を表示する

--- app/controllers/users/posts/likes_controller.rb
+++ app/controllers/users/posts/likes_controller.rb
@@ -1,4 +1,9 @@
 class Users::Posts::LikesController < Users::ApplicationController
+  def index
+    post = Post.find(params.expect(:post_id))
+    @users = post.liked_users.default_order
+  end
+
   def create
     current_user.likes.create!(post_id: params.expect(:post_id))
     redirect_to request.referer || root_path, notice: t('controllers.likes.created')
